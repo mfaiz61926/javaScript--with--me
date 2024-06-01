eb57bac2f2fcb40efb11c51c6f64dae084971129
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2024-06-01 08:29:57 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33asdf"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//in boolean  1=>true  0=>false   ""=>false   "the"=>true
+
+let str1="hello"
+let str2=" cat"
+let str3=str1+str2
+console.log(str3)
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
