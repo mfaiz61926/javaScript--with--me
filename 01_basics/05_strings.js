db32bf9402a2b8fbb6f02a485fb4b0482a9628fd
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2024-06-02 18:23:47 +0000

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name="hitesh"
+const repoCount=50
+
+//console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String(`hiteshhc`);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString=gameName.substring(0,4);
+
+const st="     test    "
+console.log(st);
+console.log(st.trim());
+
